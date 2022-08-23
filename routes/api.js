285d2a61188df01137b85919ffba0c3985c32d9c
Cody Eddie <buffbuck0427@gmail.com>
2022-08-23 17:21:05 -0600

    install dependencies

--- routes/api.js
+++ routes/api.js
@@ -24,7 +24,7 @@ router.post("/api/transaction/bulk", ({body}, res) => {
 router.get("/api/transaction", (req, res) => {
   Transaction.find({}).sort({date: -1})
     .then(dbTransaction => {
-      res.json(dbTransaction);
+      res.json(dbTransaction); 
     })
     .catch(err => {
       res.status(404).json(err);
