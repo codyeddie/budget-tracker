d730bc5a5f93082e7612087491e3780eeef3dc9d
Cody Eddie <buffbuck0427@gmail.com>
2022-08-23 18:45:07 -0600

    heroku stuff

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker"; 
 
 const app = express();
 
@@ -22,7 +22,7 @@ mongoose.connect(MONGODB_URI, {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api"))
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
